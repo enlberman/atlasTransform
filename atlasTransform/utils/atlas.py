9b8da54db1d8a530921fa064cfd7e86f9157d89b
andrew-stier <andrewstier@uchicago.edu>
2023-04-06 12:35:30 -0500

    switch coords

--- atlasTransform/utils/atlas.py
+++ atlasTransform/utils/atlas.py
@@ -41,11 +41,14 @@ def load_power() -> list:
     """
     :return:
     """
-    
-    atlas_path = os.path.join(__get_data_folder_path(), 'power_2011', 'power_order.npy')
-    atlas =  numpy.load(atlas_path,allow_pickle=True).tolist()
-    coords = [numpy.array(x.split(',')).astype(int) for x in atlas]
-    coords = [numpy.array([x[0],x[1],x[2]]) for x in coords]
+    atlas_path = os.path.join(__get_data_folder_path(), 'power_2011', 'power_dict.npy')
+    atlas = numpy.load(atlas_path, allow_pickle=True).item()
+    coords = numpy.vstack((atlas.rois['x'], atlas.rois['y'], atlas.rois['z'])).T
+
+    # atlas_path = os.path.join(__get_data_folder_path(), 'power_2011', 'power_order.npy')
+    # atlas =  numpy.load(atlas_path,allow_pickle=True).tolist()
+    # coords = [numpy.array(x.split(',')).astype(int) for x in atlas]
+    # coords = [numpy.array([x[0],x[1],x[2]]) for x in coords]
     return coords
 
 
